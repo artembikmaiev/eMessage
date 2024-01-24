22391e3b4462e7588ce23c7fc7c6b8c87e19ae03
Artem Bikmaiev <143020656+artembikmaiev@users.noreply.github.com>
2024-01-24 13:14:27 +0200

    Додано клас User (Models)

---
+++ Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace єMessage.Models
+{
+    public class User
+    {
+        public string Username { get; }
+        public string Password { get; }
+
+        public User(string username, string password)
+        {
+            Username = username;
+            Password = password;
+        }
+    }
+}
